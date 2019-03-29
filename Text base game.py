26ca13ea488921852ae0f49c2d397561ef0fb2aa
johanaverdin <48999894+johanaverdin@users.noreply.github.com>
2019-03-29 10:30:22 -0700

    Add files via upload

--- Text base game.py
+++ Text base game.py
@@ -1,10 +1,10 @@
 import time
 def welcome():
     print("Welcome to Woods adeventure!!!")
-
+####Rewrite and include the amount of can goos ypu brought
 def introinstructions():
          print(" ")
-         print("You will be in the wood for a total of two days. There you will encounter many different")
+         print("You will be in the woods for a day. There you will encounter many different")
          time.sleep(2)
          print("obstacles that you will have to over come, so prepare your bags and yourself. You will")
          time.sleep(2)
@@ -16,7 +16,7 @@ def introinstructions():
          print(" ")
 def readyresponse():
     time.sleep(3)
-    ready = input("Are you ready to begin? (Yes or No) ").capitalize()
+    ready = input("Are you ready to begin? (Yes, No) ").capitalize()
     if ready == "Yes":
         print("Lets begin!!")
     if ready == "No":
@@ -24,13 +24,13 @@ def readyresponse():
         time.sleep(4)
         print("Its time to begin.")
 def firstmove():
-    time.sleep(4)
+    time.sleep(2)
     print(" ")
     print("As you are leaving home and start walking to the woods you come across a young hurt woman. You have" )
     time.sleep(2)
-    print("the option to help her out or keep walking.")
+    print("the option to help or walk away.")
     print(" ")
-    help1 = input("Do you want to help or keep walking? (Help or Walk) ").capitalize()
+    help1 = input("Do you want to help or keep walking? (Help, Walk away) ").capitalize()
     if help1 == "Help":
         print("")
         print("The woman will need one of the only shirts to you brought along to cut the excessive blood flow")
@@ -39,17 +39,37 @@ def firstmove():
         time.sleep(2)
         print("She tells you that she has been in the woods for a day without eating anything. She asks you for food.")
         print(" ")
-    if help1 == "Walk":
-        print("Walk")
+        ifstayedtohelp()
+    if help1 == "Walk away":
+        hungrybears()
+####Come back and finsh        
 def ifstayedtohelp ():
-    food1 = input("Do you want to share 1 out of the 5 can goods you brought? (Share, Walk) ")
-   
-    
+    food1 = input("Do you want to share one of the can goods you brought? (Share, Walk) ")
+    if food1 == "Share":
+        print("You have fed her but you have lost valuable food resources.")
+             
+def hungrybears ():
+    print("You keep walking and go the wrong way. Yoou face a pair of hungry bears. Ohh no what are you going to do.")
+    print(" ")
+    bearchoice = input("You throw them two can goods but you must hide or run. What do you prefer? (Run, Hide) ").capitalize()
+    if bearchoice == "Hide":
+        print("The bears have found you. YOU ARE DEAD.")
+    if bearchoice == "Run":
+        print("Your luck. Its start raining you slip into a muddy puddle. Your backpack is open so everything falls out.")
+        pickupstuff()
+###finish   
+def pickupstuff ():
+    print(" ")
+    pickup = input("Do you want to pick up your stuff? (Yes, No) ").capitalize()
+    if pickup == "Yes":
+        print("pick up")
+    if pickup == "No":
+        print("Dont pick up")
 
 welcome()
 introinstructions()
 readyresponse()
 firstmove()
-ifstayedtohelp()
+
 
 
